fc21c8bd19f963a2e3edb29aee34e64a598bb753
blangston731 <lab09001@byui.edu>
2020-02-08 11:42:15 -0700

    .

---
+++ Lesson 4/js/message.js
@@ -0,0 +1,5 @@
+var today = new Date();
+var date = today.getDay();
+if (date == 6){
+    document.write("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.")
+}
