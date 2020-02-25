57576cd61c44255848211e210ad51fedafe22dee
blangston731 <lab09001@byui.edu>
2020-02-24 21:19:03 -0700

    .

--- Lesson 6/js/windChill.js
+++ Lesson 6/js/windChill.js
@@ -1,11 +1,9 @@
 let s = parseFloat(document.getElementById("speed").textContent); 
 let t = parseFloat(document.getElementById("temp").textContent);
-let none = "N\/A";
-let equation = (35.74 + 0.6215 * t - 35.75 * Math.pow(s, .16) + 0.4275 * t * Math.pow(s, .16)).toFixed(1);
 
 if (t > 50 || s < 3){
-document.getElementById("chill").textContent = none;
+document.getElementById("chill").textContent = "N\/A";
 }
 else {
-    document.getElementById("chill").textContent = equation;
+    document.getElementById("chill").textContent = (35.74 + 0.6215 * t - 35.75 * Math.pow(s, .16) + 0.4275 * t * Math.pow(s, .16)).toFixed(1);;
 }
