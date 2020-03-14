38ce81eae7a01621c6d0d94eb77ecfa1ef839802
blangston731 <lab09001@byui.edu>
2020-03-13 20:46:09 -0600

    .

--- lesson_10/js/weatherapi.js
+++ lesson_10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=669794a2364d1c99a3fc675b67354b5c';
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=669794a2364d1c99a3fc675b67354b5c';
 
 fetch(apiURL)
     .then(function (response) {
@@ -6,11 +6,12 @@ fetch(apiURL)
     })
     .then(function (jsObject) {
         console.log(jsObject);
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
-    });
 
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
-const desc = jsObject.weather[0].description; // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.weather[0].description; // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+    });
