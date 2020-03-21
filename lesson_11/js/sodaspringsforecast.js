555fb9ca6743c373818ec83ca78a3c0f147707d0
blangston731 <lab09001@byui.edu>
2020-03-21 12:02:02 -0600

    .

--- lesson_11/js/sodaspringsforecast.js
+++ lesson_11/js/sodaspringsforecast.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=669794a2364d1c99a3fc675b67354b5c';
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5678757&units=imperial&APPID=669794a2364d1c99a3fc675b67354b5c';
 
 fetch(apiURL)
     .then(function (response) {
@@ -6,9 +6,14 @@ fetch(apiURL)
     })
     .then(function (jsObject) {
         const fiveDays = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-        
         for (let i = 0; i < fiveDays.length; i++) {
+            var d = new Date(fiveDays[i].dt_txt);
+            const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+            document.getElementById(`day${i+1}`).textContent = weekday[d.getDay()];
+
             document.getElementById(`temp${i+1}`).textContent = fiveDays[i].main.temp;
+
             const imagesrc = 'https://openweathermap.org/img/w/' + fiveDays[i].weather[0].icon + '.png';
             const desc = fiveDays[i].weather[0].description;
             document.getElementById(`icon${i+1}`).setAttribute('src', imagesrc);
